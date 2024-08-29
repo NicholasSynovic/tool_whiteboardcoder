b54b9bea6c8f5cdaab226d2480dec19ce99fcf94
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-08-29 17:27:59 -0500

    Add llava

--- src/cli.py
+++ src/cli.py
@@ -2,14 +2,50 @@ import warnings
 from pathlib import Path
 
 import click
+import torch
 from PIL import Image
 from torch import Tensor
 from transformers import (  # DonutProcessor,
+    LlavaNextForConditionalGeneration,
+    LlavaNextProcessor,
     TrOCRProcessor,
     VisionEncoderDecoderModel,
 )
 
 warnings.filterwarnings(action="ignore")
+
+
+def llava(img: Image.Image) -> str:
+    processor = LlavaNextProcessor.from_pretrained(
+        "llava-hf/llava-v1.6-mistral-7b-hf",
+    )
+
+    model = LlavaNextForConditionalGeneration.from_pretrained(
+        "llava-hf/llava-v1.6-mistral-7b-hf",
+        torch_dtype=torch.float16,
+        low_cpu_mem_usage=True,
+    )
+    model.to("cuda")
+
+    conversation = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "image"},
+                {"type": "text", "text": "What is shown in this image?"},
+            ],
+        },
+    ]
+    prompt = processor.apply_chat_template(
+        conversation,
+        add_generation_prompt=True,
+    )
+    inputs = processor(prompt, img, return_tensors="pt").to("cuda:0")
+
+    # autoregressively complete prompt
+    output = model.generate(**inputs, max_new_tokens=100)
+
+    print(processor.decode(output[0], skip_special_tokens=True))
 
 
 def trocr(img: Image.Image) -> str:
@@ -52,7 +88,7 @@ def trocr(img: Image.Image) -> str:
 @click.option(
     "--ocr-model",
     "ocrModel",
-    type=click.Choice(choices=["trocr", "donut"], case_sensitive=False),
+    type=click.Choice(choices=["trocr", "llava"], case_sensitive=False),
     required=True,
     help="OCR (optical charachter recognition) model to use",
 )
@@ -63,6 +99,8 @@ def main(inputPath: Path, ocrModel: str) -> None:
     match ocrModel.lower():
         case "trocr":
             text = trocr(img=img)
+        case "llava":
+            text = llava(img=img)
         case _:
             print("Lol")
             quit()
